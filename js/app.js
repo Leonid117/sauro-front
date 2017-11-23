d75884d8c735ba46c972e145e5f3b706110bf011
Leonid <Leonid.v117@gmail.com>
2017-11-23 16:47:03 +0300

    home-page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-if ($(window).width() === 360) {
+if ($(window).width() <= 520) {
+
 	var items, cur, dots = $('.dots .dot');
+
 	$(document).ready(function() {
+
 		items = $('#items');
+
+		if (!items.length) {
+			return false;
+		}
+
 		items.itemslide();
+
 		cur = items.find('.itemslide-active').index() + 1;
+
 		items.on('changeActiveIndex', function(e) {
 			cur = items.find('.itemslide-active').index() + 1;
 			$.each(dots, function(i, v) {
@@ -15,5 +25,7 @@ if ($(window).width() === 360) {
 				}
 			});
 		});
+
 	});
+
 }
